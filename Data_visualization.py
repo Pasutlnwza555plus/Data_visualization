68c7f9b5c689958cefe6cd045ea60e22daeef809
KoharuL <thanaphat.manoim@gmail.com>
2025-08-04 23:59:41 +0700

    chore: test csv header

--- Data_visualization.py
+++ Data_visualization.py
@@ -726,8 +726,8 @@ elif menu == "Loss between EOL":
         st.session_state.reference_sheet = df_ref_sheet
         st.success("EOL Reference File Uploaded")
 
-    df_ref = st.session_state.reference_sheet
-    if df_ref:
+    if st.session_state.reference_sheet:
+        df_ref = st.session_state.reference_sheet
         days_count = countDay(df_ref)
         recent_rank = st.slider(label="days before", min_value=0, max_value=days_count-1, value=0)
 
