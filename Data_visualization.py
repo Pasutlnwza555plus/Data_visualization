3a1d00ad30a1db9e43157b236dfb2ee176a5b62e
KoharuL <thanaphat.manoim@gmail.com>
2025-08-04 22:40:36 +0700

    chore: test csv header

--- Data_visualization.py
+++ Data_visualization.py
@@ -619,10 +619,10 @@ elif menu == "Loss between EOL":
         try: 
             current_attentuation = float(row["Current Attenuation(dB)"])
             if float(row["Loss current - Loss EOL"]) >= 2:
-                color = ['background-color: #ff4d4d; color: #aaa'] * len(row)
+                color = ['background-color: #ff4d4d; color: white'] * len(row)
 
         except:
-            color = ['background-color: #f8d568; color: #aaa'] * len(row)
+            color = ['background-color: #e7c457; color: white'] * len(row)
         
         return color
 
