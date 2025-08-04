f97fc2a34c4a26522e3d4724fa92be6ed01a569b
KoharuL <thanaphat.manoim@gmail.com>
2025-08-04 23:32:19 +0700

    chore: test csv header

--- Data_visualization.py
+++ Data_visualization.py
@@ -648,8 +648,8 @@ elif menu == "Loss between EOL":
         st.dataframe(df_eol.style.apply(isDiffError, axis=1), hide_index=True)
         
         st.markdown("""
-            <div style='display: flex; justify-content: center; align-item: center; gap: 2px'>
-                <div style='background-color: #ff4d4d; width: 24px; height: 24px; border-radius: 2px;'></div>
+            <div style='display: flex; justify-content: center; align-items: center; gap: 2px'>
+                <div style='background-color: #ff4d4d; width: 24px; height: 24px; border-radius: 4px;'></div>
                 <div style='text-align: center; color: ff4d4d; font-size: 24px; font-weight: bold;'>
                     EOL NOT OK 
                 </div>
@@ -657,8 +657,8 @@ elif menu == "Loss between EOL":
         """, unsafe_allow_html=True)
         
         st.markdown("""
-            <div style='display: flex; justify-content: center; align-item: center; gap: 2px'>
-                <div style='background-color: #d6b346; width: 24px; height: 24px; border-radius: 2px;'></div>
+            <div style='display: flex; justify-content: center; align-items: center; gap: 2px'>
+                <div style='background-color: #d6b346; width: 24px; height: 24px; border-radius: 4px;'></div>
                 <div style='text-align: center; color: #d6b346; font-size: 24px; font-weight: bold;'>
                     Fiber break occurs
                 </div>
