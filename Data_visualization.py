60641e4a6784f8bf8fb3d3b7b7cdec267a649ea2
KoharuL <thanaphat.manoim@gmail.com>
2025-08-04 21:42:00 +0700

    chore: test csv header

--- Data_visualization.py
+++ Data_visualization.py
@@ -597,9 +597,9 @@ elif menu == "Loss between EOL":
         start = -4 - 4*recent_rank
         end_col = header_len if recent_rank == 0 else -4 * recent_rank
         header_names = df_ref.columns[start:end_col].to_list()
-        eol_ref = "EOL(dB)"
+        eol_ref = df_ref.iloc[0] == "EOL(dB)"
 
-        df_eol = pd.concat([df_ref.iloc[10], df_ref[header_names]], axis="columns")
+        df_eol = pd.concat([df_ref.iloc[eol_ref], df_ref[header_names]], axis="columns")
 
         return df_eol
 
