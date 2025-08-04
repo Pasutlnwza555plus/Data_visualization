317bd7db5acfec8da12ca23ffb153cac322ad202
KoharuL <thanaphat.manoim@gmail.com>
2025-08-04 23:26:46 +0700

    chore: test csv header

--- Data_visualization.py
+++ Data_visualization.py
@@ -648,7 +648,7 @@ elif menu == "Loss between EOL":
         st.dataframe(df_eol.style.apply(isDiffError, axis=1), hide_index=True)
         
         st.markdown("""
-            <div display='flex'>
+            <div display='flex' flex-direction='column'>
                 <div style='background-color: #ff4d4d; width: 24px; height: 24px; border-radius: 1px;'></div>
                 <div style='text-align: center; color: red; font-size: 24px; font-weight: bold;'>
                     EOL NOT OK 
