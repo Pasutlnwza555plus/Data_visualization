c7758652ccac98b68f4fcfbceae7e9953d9c9a59
KoharuL <thanaphat.manoim@gmail.com>
2025-08-06 02:04:01 +0700

    feat: calculated result df

--- Data_visualization.py
+++ Data_visualization.py
@@ -712,23 +712,17 @@ elif menu == "Loss between EOL":
         df_eol["EOL(dB)"] = df_eol_ref
         df_eol["Current Attenuation(dB)"] = df_ref[header_names[0]]
         df_eol["Loss current - Loss EOL"] = calculated_diff
-        df_eol["Remark"] = df_ref[header_names[3]]
+        df_eol["Remark"] = df_ref.get(header_names[3], "")
 
         return df_eol
         
     def isDiffError(row: pd.Series):
         color = [''] * len(row)
-
-        try:
-            if float(row["Loss current - Loss EOL"]) >= 2:
-                return ['background-color: #ff4d4d; color: white'] * len(row)
-        except:
-            pass
-
-        remark = str(row.get("Remark", "")).strip()
-        if remark != "":
-            return ['background-color: #d6b346; color: white'] * len(row)
-
+        if float(row["Loss current - Loss EOL"]) >= 2:
+            color = ['background-color: #ff4d4d; color: white'] * len(row)
+        elif row["Remark"] is not None and row["Remark"].astype(str).strip() != "":
+            color = ['background-color: #d6b346; color: white'] * len(row)
+        
         return color
     
     def countDay(df_ref: pd.DataFrame):
