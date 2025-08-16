e45836e2c6551783d74b022f7479507ff46257f1
KoharuL <thanaphat.manoim@gmail.com>
2025-08-16 16:22:17 +0700

    feat: add plain differenct of loss between core to 'Loss between Core' page.

--- pages/loss.py
+++ pages/loss.py
@@ -60,8 +60,10 @@ class LossAnalyzer:
         df_eol_ref = pd.DataFrame()
         eol_ref_columns = df_ref.columns[df_ref.iloc[0] == "EOL(dB)"]
         eol_ref_columns_float = pd.to_numeric(df_ref[eol_ref_columns[0]], downcast="float", errors="coerce")
+
         df_eol_ref["Link Name"] = df_ref['140.1'].iloc[1:]
         df_eol_ref["EOL(dB)"] = eol_ref_columns_float
+
         return df_eol_ref
 
     # Abstract placeholder (children must override)
@@ -76,11 +78,13 @@ class EOLAnalyzer(LossAnalyzer):
         df_atten = pd.DataFrame()
         source_port_col = df_raw_data["Source Port"]
         sink_port_col = df_raw_data["Sink Port"]
+
         df_atten["Link Name"] = source_port_col + "_" + sink_port_col
         df_atten["Current Attenuation(dB)"] = df_raw_data["Optical Attenuation (dB)"]
         df_atten["Remark"] = df_atten["Current Attenuation(dB)"].apply(
             lambda x: "" if self.is_castable_to_float(x) else "Fiber Break"
         )
+
         return df_atten
 
     def calculate_eol_diff(self, df_eol: pd.DataFrame) -> pd.DataFrame:
@@ -88,17 +92,26 @@ class EOLAnalyzer(LossAnalyzer):
         current_atten_col = pd.to_numeric(df_eol["Current Attenuation(dB)"], downcast="float", errors="coerce")
         eol_ref_col = pd.to_numeric(df_eol["EOL(dB)"], downcast="float", errors="coerce")
         calculated_diff = current_atten_col - eol_ref_col - 1
-        df_eol_diff["Loss current - Loss EOL"] = calculated_diff
-        ordered_cols = ["Link Name", "EOL(dB)", "Current Attenuation(dB)", "Loss current - Loss EOL", "Remark"]
-        return df_eol_diff[ordered_cols]
 
-    def process(self):
+        df_eol_diff["Loss current - Loss EOL"] = calculated_diff
+
+        ordered_cols = ["Link Name", "EOL(dB)", "Current Attenuation(dB)", "Loss current - Loss EOL", "Remark"]
+
+        return df_eol_diff[ordered_cols]
+    
+    def build_result_df(self):
         if self.df_ref is not None and self.df_raw_data is not None:
             df_eol_ref: pd.DataFrame = super().extract_eol_ref(self.df_ref)
             df_atten: pd.DataFrame   = self.extract_raw_data(self.df_raw_data)
 
             joined_df = df_eol_ref.join(df_atten.set_index("Link Name"), on="Link Name")
             df_result = self.calculate_eol_diff(joined_df)
+
+        return df_result
+
+    def process(self):
+        if self.df_ref is not None and self.df_raw_data is not None:
+            df_result = self.build_result_df()
 
             st.dataframe(df_result.style.apply(self.isDiffError, axis=1), hide_index=True)
 
@@ -107,6 +120,18 @@ class EOLAnalyzer(LossAnalyzer):
 
 # region Analyzer for Core
 class CoreAnalyzer(EOLAnalyzer):
+    def calculate_loss_between_core(self, df_result: pd.DataFrame) -> pd.DataFrame:
+        forward_direction = df_result["Loss current - Loss EOL"].iloc[::2].values
+        reverse_direction = df_result["Loss current - Loss EOL"].iloc[1::2].values
+        loss_between_core = forward_direction - reverse_direction
+
+        df_loss_between_core = pd.DataFrame(loss_between_core)
+
+        return df_loss_between_core
 
     def process(self):
-        super().process()
+        if self.df_ref is not None and self.df_raw_data is not None:
+            df_result = super().build_result_df()
+            df_loss_between_core = self.calculate_loss_between_core(df_result)
+
+            st.dataframe(df_loss_between_core)
